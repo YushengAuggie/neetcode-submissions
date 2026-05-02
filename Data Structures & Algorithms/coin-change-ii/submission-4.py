2a2f82521130fd387c63fe2f07fb4fc50cdf1121
Yusheng Ding <auggie1024.d@gmail.com>
2026-05-02 14:26:44 -0700

    Add: coin-change-ii - submission-4

---
+++ Data Structures & Algorithms/coin-change-ii/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        """
+        time: O(n * amount)
+        space: O(amount)
+        """
+        dp = [0 for _ in range(amount + 1)]
+        dp[0] = 1
+        for coin in coins:
+            for a in range(coin, amount + 1):
+                dp[a] += dp[a - coin]
+        return dp[amount]
