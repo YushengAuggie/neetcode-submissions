94932075d8d5d4fd093ccc6464375ded8839f660
Yusheng Ding <auggie1024.d@gmail.com>
2026-05-10 14:35:49 -0700

    Add: jump-game-ii - submission-8

---
+++ Data Structures & Algorithms/jump-game-ii/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        """
+        time: O(n)
+        space: O(1)
+        """
+        l = r = 0
+        fathest = 0
+        steps = 0
+        n = len(nums)
+
+        while fathest < n - 1:
+            for i in range(l, r + 1):
+                fathest = max(fathest, nums[i] + i)
+        
+            if fathest == r:
+                return -1
+            l = r + 1
+            r = fathest
+            steps += 1
+    
+        return steps
+    
+
+
