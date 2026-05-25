18a90eb301b13f63ccedc9e05d127834565ecbf6
Yusheng Ding <auggie1024.d@gmail.com>
2026-05-25 15:25:49 -0700

    Add: merge-triplets-to-form-target - submission-4

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        """
+        time: O(n)
+        space: O(1)
+        """
+        found_positions = [False] * 3
+        for t in triplets:
+            if (
+                t[0] > target[0]
+                or t[1] > target[1]
+                or t[2] > target[2]
+            ):
+                # not valid, cannot use
+                continue
+
+            for position in [0, 1, 2]:
+                if t[position] == target[position]:
+                    found_positions[position] = True
+
+        return all(found_positions)
+        
